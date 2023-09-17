453b23e8320a6258b1c49a65878ed8674a059f89
RedHotUnicorn <RedHotUnicorn@users.noreply.github.com>
2023-09-17 23:31:15 +0300

    adopt css

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,3 +4,94 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-0TPHT7KK97');
+
+
+
+
+
+
+
+
+
+
+/*SRC: https://github.com/cannibalox/logseq-custom-files*/
+
+// common =================================================================
+  MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  const watchTarget = document.getElementById("app-container"); 
+  // throttle MutationObserver 
+  // from https://stackoverflow.com/a/52868150
+    const throttle = (func, limit) => {
+      let inThrottle;
+      return (...args) => {
+        if (!inThrottle) {
+          func(...args);
+          inThrottle = setTimeout(() => (inThrottle = false), limit);
+        }
+      };
+    };
+// ================================== END COMMON
+
+// query table resizer ============================================== 
+// source : https://htmldom.dev/resize-columns-of-a-table/   
+  console.log("========= query table resizer v20220312 ============");
+
+  const createResizableColumn = function (col, resizer) {
+    // Track the current position of mouse
+    let x = 0;
+    let w = 0;
+
+    const mouseDownHandler = function (e) {
+      // Get the current mouse position
+      x = e.clientX;
+
+      // Calculate the current width of column
+      const styles = window.getComputedStyle(col);
+      w = parseInt(styles.width, 10);
+
+      // Attach listeners for document's events
+      document.addEventListener("mousemove", mouseMoveHandler);
+      document.addEventListener("mouseup", mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e) {
+      // Determine how far the mouse has been moved
+      const dx = e.clientX - x;
+      // Update the width of column
+      col.style.width = `${w + dx}px`;
+    };
+
+    // When user releases the mouse, remove the existing event listeners
+    const mouseUpHandler = function () {
+      document.removeEventListener("mousemove", mouseMoveHandler);
+      document.removeEventListener("mouseup", mouseUpHandler);
+    };
+    resizer.addEventListener("mousedown", mouseDownHandler);
+  };
+
+  const updateTables = function () {
+    // Query the table
+    const table = document.querySelectorAll(".table-auto:not(.table-resizable)");
+    for (let i = 0; i < table.length; i++) {
+      // Query all headers1
+      const cols = table[i].querySelectorAll("thead tr > th.whitespace-nowrap");
+      // Loop ver them
+      Array.from(cols).forEach((col) => {
+        // Create a resizer element
+        const resizer = document.createElement("div");
+        resizer.classList.add("query-table-resizer");
+        table[i].classList.add("table-resizable");
+        console.info("-- injected div.query-table-resizer --");
+        // Add a resizer element to the column
+        col.appendChild(resizer);
+        createResizableColumn(col, resizer);
+      });
+    }
+  };
+
+  const updateTablesThrottled = throttle(updateTables, 1000);
+  const obsTable = new MutationObserver(updateTablesThrottled);
+  obsTable.observe(watchTarget, {
+    subtree: true,
+    childList: true,
+  }); 
